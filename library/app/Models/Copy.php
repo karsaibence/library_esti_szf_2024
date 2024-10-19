c383f162c22b7d49d59c44c6b9ce61f2db1c1a59
karsaibence <karesz333@hotmail.com>
2024-10-19 17:00:49 +0200

    aasd

---
+++ library/app/Models/Copy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Copy extends Model
+{
+    /** @use HasFactory<\Database\Factories\CopyFactory> */
+    use HasFactory;
+
+    protected $primaryKey = 'copy_id';
+
+    protected $fillable = [
+        'book_id',
+        'user_id',
+    ];
+
+}
